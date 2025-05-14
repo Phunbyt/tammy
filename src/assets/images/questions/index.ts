e06fdd5c0dccf41ae22f3b1913fb5db2ed66b6f0
Oluwafunbi Raji <roluwafunbi@gmail.com>
2025-05-14 20:11:46 +0100

    modified images

--- src/assets/images/questions/index.ts
+++ src/assets/images/questions/index.ts
@@ -1,9 +1,9 @@
-import image1 from "./image1.jpg";
-import image2 from "./7d18f223-9699-429f-8d13-533b308aa62b.jpg";
-import image3 from "./c6e6594d-5c5d-4cc3-aed3-4fc9e4804385.jpg";
+import image1 from "./3a2f3d3a-e44a-4e5d-8ef2-8138c2a7fb21 copy.jpg";
+import image2 from "./7d18f223-9699-429f-8d13-533b308aa62b copy.jpg";
+import image3 from "./c6e6594d-5c5d-4cc3-aed3-4fc9e4804385 copy.jpg";
 
 import video1 from "./VIDEO-2025-05-14-14-51-07.mp4";
-import video2 from "./video2.mp4";
-import video3 from "./5e35e41d-079d-4bb9-bd3e-0a57f81a8b50.mp4";
+import video2 from "./5d507ffa-f2dc-4c77-8206-851e3adfa47e copy.mp4";
+import video3 from "./5e35e41d-079d-4bb9-bd3e-0a57f81a8b50 copy.mp4";
 
 export { image1, image2, image3, video1, video2, video3 };
